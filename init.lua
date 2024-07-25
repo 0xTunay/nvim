1ae6d995f7c0228890fdec8bb41e9fc245f1eab7
Ttunay <tigrus232323k@gmail.com>
2024-07-25 17:27:40 +0300

    add lua neovim

---
+++ init.lua
@@ -0,0 +1,11 @@
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+
+
+-- PLUGINS
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.dashboard')
+require('plugins.cmp')
---
+++ lua/core/plugins.lua
@@ -0,0 +1,58 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/nvim-cmp'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/cmp-buffer'},
+  {'hrsh7th/cmp-path'},
+  {'hrsh7th/cmp-cmdline'},
+  {'L3MON4D3/LuaSnip'},
+  {'saadparwaiz1/cmp_luasnip'},
+
+    {'shaunsingh/nord.nvim'},
+    {'rebelot/kanagawa.nvim'},
+    { 'wakatime/vim-wakatime', lazy = false },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons",
+          "MunifTanjim/nui.nvim",
+          's1n7ax/nvim-window-picker',
+
+        }
+    },
+ {
+  "nvim-treesitter/nvim-treesitter",
+  opts = { ensure_installed = { "cpp" } },
+},
+{'tree-sitter/tree-sitter-c'},
+{'neovim/nvim-lspconfig'},
+{
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+},
+{'shaunsingh/nord.nvim'},
+})
