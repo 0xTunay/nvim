0e5c8ddccad47af4b5f2269bf1eb2ff966747c62
0xTunay <117542161+0xTunay@users.noreply.github.com>
2024-08-13 13:12:13 +0300

    Update init.lua
    edit tabulation

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ require('core.plugins')
 require('core.mappings')
 require('core.colors')
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 
 -- PLUGINS
 require('plugins.neotree')
